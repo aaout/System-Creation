0a8ff56df1d38fc1f058dc307112a36d65579ce3
aaout <tiktik_suke@outlook.jp>
2020-05-28 07:24:56 +0900

    delete array and create class

--- task2/task2-1.py
+++ task2/task2-1.py
@@ -2,55 +2,62 @@ import matplotlib.pyplot as plt
 import numpy as np
 
 
+#ノードの状態を確保するクラスを作成
+class Node:
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
+        self.wall = True
+        self.check = False
+
+
 def main():
     #地図の取得
     maze = np.loadtxt('task2.txt', dtype=int)
-    print(len(maze))
-    #地図の縦と横の長さ
-    height = maze.shape[0]-1
-    width = maze.shape[1] - 1
-
-    #ゴールは一マス内側
-    goal_x = height-1
-    goal_y = width - 1
+    length = len(maze)
 
     #各リストの作成
-    openlist = [[1,1]]
-    closed = []
+    openlist = []
+    closedlist = []
+    maze_node = [[Node(i, j) for j in range(length)] for i in range(length)]
     dx_dy = [[1, 0], [0, 1], [-1, 0], [0, -1]]
+
+    for j in range(length):
+        for i in range(length):
+            if maze[i][j] == 0:
+                maze_node[i][j].wall = False
+    openlist.append(maze_node[1][1])
+    maze_node[1][1].check = True
 
     #地図の下準備
     plt.imshow(maze, cmap="binary")
-    plt.xticks(rotation=90)
-    plt.xticks(np.arange(width), np.arange(width))
-    plt.yticks(np.arange(height), np.arange(height))
+    plt.xticks(np.arange(length), np.arange(length))
+    plt.yticks(np.arange(length), np.arange(length))
     plt.plot(1, 1, "D", color="tab:red", markersize=10)
-    plt.plot(goal_x, goal_y, "D", color="tab:green", markersize=10)
-    maze[1][1] = 1
+    plt.plot(length-2, length-2, "D", color="tab:green", markersize=10)
 
     #オープンリストが空になるまで
     while openlist:
         #注目しているマス目
         target = openlist.pop()
-        closed.append(target)
+        closedlist.append(target)
+        # print(target.x)
 
         #ゴールに着いたら終わり
-        if target[0] == goal_x and target[1] == goal_y:
+        #注目マスの前後左右を探索
+        #未チェックマスならチェックしてオープンリストに追加
+        #ついでにベクトルもここでプロットしておく
+        if (target.x) == length - 2 and (target.y) == length - 2:
             break
         for i in range(4):
-            #注目マスの前後左右を探索
-            #未チェックマスならチェックしてオープンリストに追加
-            #ついでにベクトルもここでプロットしておく
-            nx, ny = target[0] + dx_dy[i][0], target[1] + dx_dy[i][1]
-            if 1 <= nx < height and 1 <= ny < width and maze[nx][ny] == 0:
-                openlist.append([nx, ny])
-                plt.quiver(target[1], target[0], ny-target[1], nx-target[0], angles='xy', scale_units='xy', scale=1)
-                maze[nx][ny] = 1
+            nx, ny = target.x + dx_dy[i][0], target.y + dx_dy[i][1]
+            if maze_node[nx][ny].wall == False and maze_node[nx][ny].check == False and 1 <= nx < (length - 1) and 1 <= ny < (length - 1):
+                openlist.append(maze_node[nx][ny])
+                maze_node[nx][ny].check = True
+                plt.quiver(target.y, target.x, ny - (target.y), nx - (target.x), angles='xy', scale_units='xy', scale=1)
 
     #図と最終的なクローズドリストの表示
     plt.show()
-    print("探索結果")
-    print(closed)
 
 if __name__ == "__main__":
     main()
--- task3/task3-1.py
+++ task3/task3-1.py
@@ -2,6 +2,7 @@ import matplotlib.pyplot as plt
 import numpy as np
 
 
+#ノードの状態を確保するクラスを作成
 class Node:
     def __init__(self, x, y):
         self.x = x
@@ -11,8 +12,11 @@ class Node:
 
 
 def main():
+    #地図の取得
     maze = np.loadtxt('task3.txt', dtype=int)
-    length = 3
+    length = len(maze)
+
+    #各リストの作成
     openlist = []
     closedlist = []
     maze_node = [[Node(i, j) for j in range(length)] for i in range(length)]
@@ -32,17 +36,28 @@ def main():
     plt.plot(1, 1, "D", color="tab:red", markersize=10)
     plt.plot(length-2, length-2, "D", color="tab:green", markersize=10)
 
+    #オープンリストが空になるまで
+    while openlist:
+        #注目しているマス目
+        target = openlist.pop()
+        closedlist.append(target)
+        # print(target.x)
 
-    # while openlist:
-    #     target = openlist.pop()
-    #     closedlist.append(target)
+        #ゴールに着いたら終わり
+        #注目マスの前後左右を探索
+        #未チェックマスならチェックしてオープンリストに追加
+        #ついでにベクトルもここでプロットしておく
+        if (target.x) == length - 2 and (target.y) == length - 2:
+            break
+        for i in range(4):
+            nx, ny = target.x + dx_dy[i][0], target.y + dx_dy[i][1]
+            if maze_node[nx][ny].wall == False and maze_node[nx][ny].check == False and 1 <= nx < (length - 1) and 1 <= ny < (length - 1):
+                openlist.append(maze_node[nx][ny])
+                maze_node[nx][ny].check = True
+                plt.quiver(target.y, target.x, ny - (target.y), nx - (target.x), angles='xy', scale_units='xy', scale=1)
 
-    #     for i in range(4):
-    #         nx, ny = target.x + dx_dy[i][0], target.y + dx_dy[i][1]
-    #         if maze_node[nx][ny].wall == False and maze_node[nx][ny].check == False:
-    #             openlist.append(maze_node(nx, ny))
-    #             maze_node[nx][ny].check = True
-
+    #図と最終的なクローズドリストの表示
+    plt.show()
 
 if __name__ == "__main__":
     main()
