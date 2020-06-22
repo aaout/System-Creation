f9647df614003ebcacaf6c222cba2c18d152e3fb
aaout <tiktik_suke@outlook.jp>
2020-06-23 04:38:53 +0900

    test

--- test.py
+++ test.py
@@ -1,11 +1,7 @@
 import numpy as np
 import sympy as sym
 
-a, b = sym.symbols("a, b")
-eq = 1/2 *((b)**2 + (a - 1/2)**2 + (a  - 3/2)**2)
-eq1 = sym.diff(eq, a)
-eq2 = sym.diff(eq, b)
-
-ans = sym.solve([eq1, eq2])
-print("a:",float(ans[a]))
-print("b:",float(ans[b]))
+x = [2, 0, 0, 0, 0, 0, 1, 3, 4]
+print(x)
+x.remove(0)
+print(x)
