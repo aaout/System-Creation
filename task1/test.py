a1fc5b8a8af33f0d04c58896f7469ab1fd729df5
aaout <tiktik_suke@outlook.jp>
2020-06-27 14:23:38 +0900

    test

--- task1/test.py
+++ task1/test.py
@@ -1,2 +1,19 @@
-x, y, z = map(int, input().split())
-print(z,x,y)
+import matplotlib.pyplot as plt
+import numpy as np
+
+x = np.array([-1,0,1,2,2])
+y = np.array([-1,0,1,3,1])
+
+def reg1dim(x, y):
+    n = len(x)
+    a = ((np.dot(x, y)- y.sum() * x.sum()/n)/
+        ((x ** 2).sum() - x.sum()**2 / n))
+    b = (y.sum() - a * x.sum())/n
+    return a, b
+
+a, b = reg1dim(x, y)
+print(a, b)
+
+plt.scatter(x, y, color="k")
+plt.plot([0, x.max()], [b, a * x.max() + b]) #(0, b)地点から(xの最大値,ax + b)地点までの線
+plt.show()
--- task4/task4-2.py
+++ task4/task4-2.py
@@ -31,13 +31,6 @@ def main():
             if maze[i][j] == 0:
                 maze_node[i][j].wall = False
 
-    # 地図の下準備
-    # plt.imshow(maze, cmap="binary")
-    plt.xticks(np.arange(length), np.arange(length))
-    plt.yticks(np.arange(length), np.arange(length))
-    plt.plot(1, 1, "D", color="tab:red", markersize=10)
-    plt.plot(length - 2, length - 2, "D", color="tab:green", markersize=10)
-
     #行動選択
     dx_dy = [[0, 0], [0, 1], [1, 0], [0, -1], [-1, 0]]
     #行動回数(停止も含める)
@@ -61,11 +54,6 @@ def main():
                     a = dx_dy[i]
                     nextTarget = maze_node[target.x + a[0]][target.y + a[1]]
                     next_maxQ = max(nextTarget.Q)
-                    # if nextTarget == maze_node[13][13]:
-                    #     td_error = (10 + gamma * next_maxQ) - target.Q[i]
-                    #     target.Q[i] = alpha * td_error
-                    #     print(next_maxQ)
-                    # else:
                     if nextTarget.wall:
                         td_error = (-1 + gamma * next_maxQ) - target.Q[i]
                         target.Q[i] += alpha * td_error
@@ -83,19 +71,35 @@ def main():
                     target = nextTarget
 
 
-                # max_value = max(target.Q)
-                # index_list = []
-                # for i in range(5):
-                #     if target.Q[i] == max_value:
-                #         index_list.append(i)
-                # a = dx_dy[random.choice(index_list)]
-                # nextTarget = maze_node[target.x + a[0]][target.y + a[1]]
-                # target = nextTarget
 
-    # print(maze_node[11][12].Q)
     Q_table = [[max(maze_node[i][j].Q) for j in range(length)] for i in range(length)]
+    # 地図の下準備
+    # plt.imshow(maze, cmap="binary")
+    plt.figure()
+    plt.xticks(np.arange(length), np.arange(length))
+    plt.yticks(np.arange(length), np.arange(length))
+    plt.plot(1, 1, "D", color="tab:red", markersize=10)
+    plt.plot(length - 2, length - 2, "D", color="tab:green", markersize=10)
     plt.imshow(Q_table, cmap="Greens")
+
+    # plt.figure()
+    # dx_dy = [[1, 0], [0, 1], [-1, 0], [0, -1]]
+    # Q_target = [1, 1]
+    # while Q_target != [length - 2, length - 2]:
+    #     const = Q_table[Q_target[0]][Q_target[1]]
+    #     next_x, next_y = 0, 0
+    #     for i in range(4):
+    #         nx, ny = Q_target[0] + dx_dy[i][0], Q_target[1] + dx_dy[i][1]
+    #         if Q_table[nx][ny] >= const:
+    #             const = Q_table[nx][ny]
+    #             next_x, next_y = nx, ny
+    #     plt.quiver(Q_target[1], Q_target[0], next_y - (Q_target[1]), next_x - (Q_target[0]), angles='xy', scale_units='xy', scale=1)
+    #     Q_target = Q_table[next_x][next_y]
+
+
     plt.show()
+
+
 
 
 
