4a52d26ed362f0fdddc919d554d79fbde24bea79
aaout <tiktik_suke@outlook.jp>
2020-07-07 19:06:56 +0900

    線形計画

--- test2.py
+++ test2.py
@@ -2,20 +2,28 @@ import matplotlib.pyplot as plt
 import numpy as np
 from mpl_toolkits.mplot3d import Axes3D
 
-# (x, y, z)
-x = [1, 2, 3, 4, 5]
-y = [1, 2, 3, 4, 5]
-z = [1, 2, 3, 4, 5]
 
-# 3Dでプロット
+def func0(x, y):
+    return -x - 1/2 * y + 4
+def func1(x, y):
+    return -2 * x - y + 6
+def func2(x, y):
+    return - 3/2 * x - 3 * y + 9
+
+x = np.arange(-10, 10, 0.5)
+y = np.arange(-10, 10, 0.5)
+
+X, Y = np.meshgrid(x, y)
+Z_0 = func0(X, Y)
+Z_1 = func1(X, Y)
+Z_2 = func2(X, Y)
+
 fig = plt.figure()
 ax = Axes3D(fig)
-ax.plot(x, y, z, "o-", color="#00aa00", ms=4, mew=0.5)
-
-# 軸ラベル
-ax.set_xlabel('x')
-ax.set_ylabel('y')
-ax.set_zlabel('z')
-
-# 表示
+ax.set_xlabel("x")
+ax.set_ylabel("y")
+ax.set_zlabel("f(x, y)")
+ax.plot_wireframe(X, Y, Z_0, color = 'red')
+ax.plot_wireframe(X, Y, Z_1, color = 'blue')
+ax.plot_wireframe(X, Y, Z_2, color = 'green')
 plt.show()
